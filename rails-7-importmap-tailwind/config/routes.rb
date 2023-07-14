ca09fac1a2be57d387370c54a4cf954e1a56e6a9
Elias Rodrigues <eliasrodrigues.info@gmail.com>
2023-07-14 12:07:19 -0300

    Run bundle exec rails generate administrate:install

--- rails-7-importmap-tailwind/config/routes.rb
+++ rails-7-importmap-tailwind/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :products
+
+      root to: "products#index"
+    end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
